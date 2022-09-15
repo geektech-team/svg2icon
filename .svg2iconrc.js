e0bdd9c2fd1d8defe013f91fbaaf999545fbc183
Jimmie Max <mayiming@morefungroup.com>
2022-09-15 14:13:18 +0800

    fix(css): fix font-face

--- .svg2iconrc.js
+++ .svg2iconrc.js
@@ -4,5 +4,6 @@ module.exports = {
     src: "svg", // svg path(svg资源路径)
     fontName: "geek-icon", // font name （字体名称）
     classNamePrefix: "geekicon", // class name prefix （class前缀）
+    linkMode: 'inline' // inline\link
   }
   
