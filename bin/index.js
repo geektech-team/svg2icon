2a96d7b65721eca883defac4e73dd1e2d3024139
Jimmie Max <mayiming@morefungroup.com>
2022-09-16 14:39:20 +0800

    feat(bin): remove default

--- bin/index.js
+++ bin/index.js
@@ -3,16 +3,8 @@ const fs = require("fs");
 const path = require("path");
 const svg2icon = require('../src/index.js')
 
-const defaultOptions = {
-  dist: "dist", // output path(输出文件路径)
-  src: "svg", // svg path(svg资源路径)
-  fontName: "geek-icon", // font name （字体名称）
-  classNamePrefix: "geekicon", // class name prefix （class前缀）
-};
 let options = {};
-let inputOptions = {};
 if (fs.existsSync(path.resolve(".svg2iconrc.js"))) {
-  inputOptions = require(path.resolve(".svg2iconrc.js")) || {};
+  options = require(path.resolve(".svg2iconrc.js")) || {};
 }
-options = { ...defaultOptions, ...inputOptions };
 svg2icon(options);
