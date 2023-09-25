dfe63f418299c1cf7b6e0e617154d40b4036c271
Jimmie Max <mayiming@morefungroup.com>
2023-09-25 16:58:06 +0800

    feat(js): rc.js to json

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,9 @@ const path = require("path");
 const svg2icon = require('../src/index.js')
 
 let options = {};
-if (fs.existsSync(path.resolve(".svg2iconrc.js"))) {
+if (fs.existsSync(path.resolve(".svg2iconrc.json"))) {
+  options = require(path.resolve(".svg2iconrc.json")) || {};
+} else if (fs.existsSync(path.resolve(".svg2iconrc.js"))) {
   options = require(path.resolve(".svg2iconrc.js")) || {};
 } else if (fs.existsSync(path.resolve(".svg2iconrc.cjs"))) {
   options = require(path.resolve(".svg2iconrc.cjs")) || {};
